191f124763ec74e74b3002f49957b97038702f16
myname <adiy.personal.mail@ya.ru>
2021-11-19 15:46:41 +0300

    chore: minor fix

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,14 +1,12 @@
-import { stopSubmit } from "redux-form";
-import { authAPI, securityAPI } from "../api/api";
+import { stopSubmit } from 'redux-form';
+import { authAPI, securityAPI } from '../api/api';
 
-const SET_USER_DATA = "react-first/auth/SET_USER_DATA";
-const GET_CAPTCHA_URL_SUCCESS = "samurai-network/auth/GET_CAPTCHA_URL_SUCCESS";
-let initialState = {
+const initialState = {
   userId: null,
   email: null,
   login: null,
   isAuth: false,
-  captchaUrl: null, // if null, then captcha is not required
+  captchaUrl: null
 };
 
 const authReducer = (state = initialState, action) => {
@@ -17,61 +15,62 @@ const authReducer = (state = initialState, action) => {
     case GET_CAPTCHA_URL_SUCCESS:
       return {
         ...state,
-        ...action.payload, //так как данные свойства находятся ниже они перезатруд которые выше в ...state
+        ...action?.payload
       };
     default:
       return state;
   }
 };
 
+const SET_USER_DATA = 'react-first/auth/SET_USER_DATA';
 export const setAuthUserData = (userId, email, login, isAuth) => ({
   type: SET_USER_DATA,
-  payload: { userId, email, login, isAuth },
-});
-export const getCaptchaUrlSuccess = (captchaUrl) => ({
-  type: GET_CAPTCHA_URL_SUCCESS,
-  payload: { captchaUrl },
+  payload: { userId, email, login, isAuth }
 });
 
-export const getAuthUserData = () => async (dispatch) => {
-  let response = await authAPI.me();
+const GET_CAPTCHA_URL_SUCCESS = 'samurai-network/auth/GET_CAPTCHA_URL_SUCCESS';
+export const getCaptchaUrlSuccess = captchaUrl => ({
+  type: GET_CAPTCHA_URL_SUCCESS,
+  payload: { captchaUrl }
+});
+
+export const getAuthUserData = () => async dispatch => {
+  const response = await authAPI.me();
 
   if (response.data.resultCode === 0) {
-    let { id, email, login } = response.data.data;
+    const { id, email, login } = response.data.data;
     dispatch(setAuthUserData(id, email, login, true)); //first data from axios
   }
 };
 
 export const login =
-  (email, password, rememberMe, captcha) => async (dispatch) => {
-    let response = await authAPI.login(email, password, rememberMe, captcha);
+  (email, password, rememberMe, captcha) => async dispatch => {
+    const response = await authAPI.login(email, password, rememberMe, captcha);
     if (response.data.resultCode === 0) {
-      // success, get auth data
       dispatch(getAuthUserData());
     } else {
       if (response.data.resultCode === 10) {
         dispatch(getCaptchaUrl());
       }
 
-      let message =
+      const message =
         response.data.messages.length > 0
           ? response.data.messages[0]
-          : "Some error";
-      dispatch(stopSubmit("login", { _error: message }));
+          : 'Some error';
+      dispatch(stopSubmit('login', { _error: message }));
     }
   };
 
-export const getCaptchaUrl = () => async (dispatch) => {
+export const getCaptchaUrl = () => async dispatch => {
   const response = await securityAPI.getCaptchaUrl();
   const captchaUrl = response.data.url;
   dispatch(getCaptchaUrlSuccess(captchaUrl));
 };
 
-export const logout = () => async (dispatch) => {
-  let response = await authAPI.logout();
+export const logout = () => async dispatch => {
+  const response = await authAPI.logout();
 
   if (response.data.resultCode === 0) {
-    //статус 0 тогда всё хорошо
     dispatch(setAuthUserData(null, null, null, false));
   }
 };
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,22 +1,21 @@
-import { combineReducers, createStore, compose, applyMiddleware } from "redux";
-import profileReducer from "./profile-reducer";
-import dialogsReducer from "./dialogs-reducer";
-import sidebarReducer from "./sidebar-reducer";
-import usersReducer from "./users-reducer";
-import authReducer from "./auth-reducer";
-import { reducer as formReducer } from "redux-form";
-import appReducer from "./app-reducer";
-import thunkMiddleware from "redux-thunk";
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
+import profileReducer from './profile-reducer';
+import dialogsReducer from './dialogs-reducer';
+import sidebarReducer from './sidebar-reducer';
+import usersReducer from './users-reducer';
+import authReducer from './auth-reducer';
+import { reducer as formReducer } from 'redux-form';
+import appReducer from './app-reducer';
+import thunkMiddleware from 'redux-thunk';
 
 let reducers = combineReducers({
-  // воспринимает всё в combineReducers how state
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarReducer,
   usersPage: usersReducer,
   auth: authReducer,
   form: formReducer,
-  app: appReducer,
+  app: appReducer
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
