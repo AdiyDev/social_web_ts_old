5ec88b8802aefe22bbf12565d7f8bb0574e79a5d
myname <adiy.personal.mail@ya.ru>
2021-11-26 15:17:24 +0300

    feat: add ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { Component, Suspense } from 'react'
+
 import './App.css'
+import 'antd/dist/antd.css'
 import HeaderContainer from './components/Header/HeaderContainer'
 import Navbar from './components/Navbar/Navbar'
 import News from './components/News/News'
@@ -19,6 +21,15 @@ import { initializeApp } from './redux/app-reducer'
 import { compose } from 'redux'
 import Preloader from './components/common/Preloader/Preloader'
 import store, { AppStateType } from './redux/redux-store'
+import { Layout, Menu, Breadcrumb } from 'antd'
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined
+} from '@ant-design/icons'
+
+const { SubMenu } = Menu
+const { Header, Content, Footer, Sider } = Layout
 
 const DialogsContainer = React.lazy(
   () => import('./components/Dialogs/DialogsContainer')
@@ -51,42 +62,106 @@ class App extends Component<MapPropsType & DispatchPropsType> {
       return <Preloader />
     }
     return (
-      <div className="app-wrapper">
-        <HeaderContainer />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Suspense
-            fallback={
-              <div>
-                <Preloader />
-                Загружка.......
-              </div>
-            }
+      // <div className="app-wrapper">
+      //   <HeaderContainer />
+      //   <Navbar />
+      //   <div className="app-wrapper-content">
+      //
+      //   </div>
+      // </div>
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Layout
+            className="site-layout-background"
+            style={{ padding: '24px 0' }}
           >
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={() => <Redirect to={'/profile'} />}
-              />
-              <Route
-                path="/profile/:userId?"
-                render={() => <ProfileContainer />}
-              />
-              <Route path="/dialogs" render={() => <DialogsContainer />} />
-              <Route path="/news" render={() => <News />} />
-              <Route path="/music" render={() => <Music />} />
-              <Route
-                path="/users"
-                render={() => <UsersPage pageTitle={'Заголовок Страницы'} />}
-              />
-              <Route path="/settings" render={() => <Settings />} />
-              <Route path="/login" render={() => <LoginPage />} />
-              <Route path="*" render={() => <div>404 NOT FOUND _ERROR</div>} />
-            </Switch>
-          </Suspense>
-        </div>
-      </div>
+            <Sider className="site-layout-background" width={200}>
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{ height: '100%' }}
+              >
+                <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+                  <Menu.Item key="1">option1</Menu.Item>
+                  <Menu.Item key="2">option2</Menu.Item>
+                  <Menu.Item key="3">option3</Menu.Item>
+                  <Menu.Item key="4">option4</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+                  <Menu.Item key="5">option5</Menu.Item>
+                  <Menu.Item key="6">option6</Menu.Item>
+                  <Menu.Item key="7">option7</Menu.Item>
+                  <Menu.Item key="8">option8</Menu.Item>
+                </SubMenu>
+                <SubMenu
+                  key="sub3"
+                  icon={<NotificationOutlined />}
+                  title="subnav 3"
+                >
+                  <Menu.Item key="9">option9</Menu.Item>
+                  <Menu.Item key="10">option10</Menu.Item>
+                  <Menu.Item key="11">option11</Menu.Item>
+                  <Menu.Item key="12">option12</Menu.Item>
+                </SubMenu>
+              </Menu>
+            </Sider>
+            <Content style={{ padding: '0 24px', minHeight: 280 }}>
+              <Suspense
+                fallback={
+                  <div>
+                    <Preloader />
+                    Загружка.......
+                  </div>
+                }
+              >
+                <Switch>
+                  <Route
+                    exact
+                    path="/"
+                    render={() => <Redirect to={'/profile'} />}
+                  />
+                  <Route
+                    path="/profile/:userId?"
+                    render={() => <ProfileContainer />}
+                  />
+                  <Route path="/dialogs" render={() => <DialogsContainer />} />
+                  <Route path="/news" render={() => <News />} />
+                  <Route path="/music" render={() => <Music />} />
+                  <Route
+                    path="/users"
+                    render={() => (
+                      <UsersPage pageTitle={'Заголовок Страницы'} />
+                    )}
+                  />
+                  <Route path="/settings" render={() => <Settings />} />
+                  <Route path="/login" render={() => <LoginPage />} />
+                  <Route
+                    path="*"
+                    render={() => <div>404 NOT FOUND _ERROR</div>}
+                  />
+                </Switch>
+              </Suspense>
+            </Content>
+          </Layout>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
     )
   }
 }
