e0fbc8fae7cde170c817011e0e1023d339a7d406
AdiyDev <kain011@ya.ru>
2021-11-28 18:52:27 +0300

    fix: minor fix

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -50,7 +50,8 @@ const Messages: React.FC = () => {
 const Message: React.FC<{ message: ChatMessageType }> = ({ message }) => {
   return (
     <div>
-      <img style={{ width: '30px', height: 30 }} src={message?.photo} alt="" />\
+      <img style={{ width: '30px', height: 30 }} src={message?.photo} alt="" />
+      <br />
       <i>{message?.userId}</i>
       <b>{message?.userName}</b>
       <br />
